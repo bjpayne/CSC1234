43523e239f5fdaa441229b1b32aa6f27029a5bd2
Ben <bj.payne@me.com>
2019-10-07 19:48:48 -0400

    dev commit

--- TicTacToe/TicTacToe.cs
+++ TicTacToe/TicTacToe.cs
@@ -5,32 +5,55 @@ namespace TicTacToe
 {
     class TicTacToe
     {
-        private readonly String[,] gameBoard;
+        private readonly Int32[,] gameBoard;
+
+        private String[,] moves;
 
         public TicTacToe()
         {
-            this.gameBoard = new String[,] {{"", "", ""}, {"", "", ""}, {"", "", ""}};
+            this.gameBoard = new Int32[,] {
+				{0, 1, 2},
+				{3, 4, 5},
+				{6, 7, 8},
+				{0, 3, 6},
+				{1, 4, 7},
+				{2, 5, 8},
+				{0, 4, 8},
+				{2, 4, 6},
+			};
         }
 
         public void Move(Int32 iPosition, Int32 jPosition, Player player)
         {
-            this.gameBoard[iPosition, jPosition] = player.Symbol;
+            this.moves[iPosition, jPosition] = player.Symbol;
         }
 
         public Boolean IsWin(Player player)
         {
-            for (Int32 i = 0; i <= 2; i++)
+            Boolean isWin = false;
+            
+            for (Int32 i = 0; i < 8; i++)
             {
-                for (Int32 j = 0; j <= 2; j++)
+                Int32 cell1 = gameBoard[i, 0];
+                Int32 cell2 = gameBoard[i, 1];
+                Int32 cell3 = gameBoard[i, 2];
+
+                if (moves[i, 0] == "" || moves[i, 1] == "" || moves[i, 2] == "")
                 {
-                    if (player.Symbol.Equals(this.gameBoard[i,j]))
-                    {
-                        player.Hits++;
-                    }
+                    continue;
+                }
+
+                if (moves[i, 0] == player.Symbol && 
+                    moves[i, 1] == player.Symbol && 
+                    moves[i, 2] == player.Symbol)
+                {
+                    isWin = true;
+
+                    break;
                 }
             }
-            
-            return player.Hits == 3;
+
+            return isWin;
         }
     }
 }
